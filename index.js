3a2a63c5aff81b84826229c8ca03c8c61f1f3214
wujingquan <i@wujingquan.com>
2022-02-14 13:17:35 +0800

    putStream成功后返回的size为0 (#2)
    * 支持文件自定义路径，返回文件大小（meta调用获取)
    
    * 无文件扩展名时的bug.1
    
    * fix size=0
    
    * 相关属性跟官方multer对齐
    
    * update README.md
    
    * update
    
    Co-authored-by: lls <lls@gddata.net>

--- index.js
+++ index.js
@@ -6,55 +6,95 @@
 const OSS = require('ali-oss');
 const crypto = require('crypto');
 
-const getFilename = (req, file, cb) => {
-	crypto.pseudoRandomBytes(16, (err, raw) => {
-		cb(err, err ? undefined :
-			(raw.toString('hex') + file.originalname.substr(file.originalname.lastIndexOf('.')))
-		);
-	});
+
+const ERROR_MESSAGE = 'oss client undefined'
+
+const getFilename = ({ req, file, ossPath }) => {
+    return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, raw) => {
+            err && reject(err)
+            let ext = '';
+            if (file.originalname.indexOf(".") > -1) {
+                ext = file.originalname.substr(file.originalname.lastIndexOf('.'));
+            }
+            let name = raw.toString('hex') + ext;
+            resolve(name)
+        });
+    })
 };
 
+const getPath = (path) => {
+    return function() {
+        return Promise.resolve(path)
+    }
+}
+
 class AliYunOssStorage {
-	constructor(opts) {
-		this.client = new OSS(opts.config);
-		this.getFilename = opts.filename || getFilename;
-	}
 
-	_handleFile(req, file, cb) {
-		if (!this.client) {
-			console.error('oss client undefined');
-			return cb({message: 'oss client undefined'});
-		}
-		this.getFilename(req, file, (err, filename) => {
-			if (err) return cb(err);
-			this.client.putStream(filename, file.stream).then(
-				result => {
-					return cb(null, {
-						filename: result.name,
-						url     : result.url
-					});
-				}
-			).catch(err => {
-				return cb(err);
-			});
-		});
-	}
+    constructor(opts) {
+        if (opts.config.path && typeof opts.config.path === 'string') {
+            this.getPath = getPath(opts.config.path)
+        } else if(opts.config.path && typeof opts.config.path === 'function') {
+            this.getPath = opts.config.path
+        } else {
+            this.getPath = getPath('')
+        }
 
-	_removeFile(req, file, cb) {
-		if (!this.client) {
-			console.error('oss client undefined');
-			return cb({message: 'oss client undefined'});
-		}
-		this.client.delete(file.filename).then(
-			result => {
-				return cb(null, result);
-			}
-		).catch(err => {
-			return cb(err);
-		});
-	}
+        this.client = new OSS(opts.config);
+        this.getFilename = opts.filename || getFilename;
+    }
+
+    _handleFile(req, file, cb) {
+        if (!this.client) {
+            console.error(ERROR_MESSAGE);
+            return cb({ message: ERROR_MESSAGE });
+        }
+        let ossPath, filename, url, size;
+        this.getPath({ req, file })
+            .then(_ossPath => {
+                ossPath = _ossPath
+                return this.getFilename({ req, file })
+            })
+            .then(_filename => {
+                filename = _filename
+                return this.client.putStream(ossPath + '/' + filename, file.stream)
+            })
+            .then(({ url: _url, name }) => {
+                url = _url
+                return this.client.getObjectMeta(name)
+            })
+            .then(meta => {
+                size = (meta && meta.status && meta.res && meta.res.headers) ? meta.res.headers["content-length"] : null
+                return cb(null, {
+                    destination: url.substr(0, url.lastIndexOf('/')),
+                    filename,
+                    path: url,
+                    size
+                })
+            })
+            .catch(err => {
+                return cb(err)
+            })
+    }
+
+    _removeFile(req, file, cb) {
+        if (!this.client) {
+            console.error(ERROR_MESSAGE);
+            return cb({ message: ERROR_MESSAGE });
+        }
+        this.client.delete(file.filename).then(
+            result => {
+                return cb(null, result);
+            }
+        ).catch(err => {
+            return cb(err);
+        });
+    }
 }
 
 module.exports = function (opts) {
-	return new AliYunOssStorage(opts);
+    if (typeof opts === 'object' && opts !== null) {
+        return new AliYunOssStorage(opts);
+    }
+    throw new TypeError('Expected object for argument options');
 };
